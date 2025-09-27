a6271ce1a6880d1e2cfbff13053b11362a12c02c
coggiee <zentechie7@gmail.com>
2025-09-28 00:06:39 +0900

    change getURL method logic

--- src/authProvider.ts
+++ src/authProvider.ts
@@ -3,10 +3,10 @@ import { supabaseClient } from "./utility";
 import type { AuthUser } from "./types/auth";
 
 const getURL = () => {
-  let url =
-    import.meta.env.VITE_SITE_URL ?? // Set this to your site URL in production env.
-    import.meta.env.VITE_VERCEL_URL ?? // Automatically set by Vercel.
-    "http://localhost:3000/";
+  let url = import.meta.env.DEV
+    ? "http://localhost:5173/"
+    : import.meta.env.VITE_SITE_URL ?? // Set this to your site URL in production env.
+      import.meta.env.VITE_VERCEL_URL; // Automatically set by Vercel.
   // Make sure to include `https://` when not localhost.
   url = url.startsWith("http") ? url : `https://${url}`;
   // Make sure to include a trailing `/`.
